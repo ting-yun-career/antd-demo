e1b1ae2d52e8c5bdc01cef79c5863c03e1b265f0
Ting Yun <ting.yun@binance.comm>
2023-04-30 14:36:41 -0700

    add build folder and antd button demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import type { FC } from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
 
-function App() {
-  return (
-    <div>
-      <div>content</div>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
 
 export default App;

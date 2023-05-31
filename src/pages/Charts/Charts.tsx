e1b994561a107cc8810bc3fff489412b2579fe26
Ting Yun <ting.yun@binance.com>
2023-05-31 11:09:48 -0700

    replace g2 / g2-react with @antv/g2 package get interval chart working with dark mode toggle

--- src/pages/Charts/Charts.tsx
+++ src/pages/Charts/Charts.tsx
@@ -1,17 +1,84 @@
-const Charts: React.FC = () => {
-  const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
-  ]
+import { Chart } from '@antv/g2'
+import { useContext, useEffect, useRef } from 'react'
+import { GlobalContext } from '../../global/globalProvider'
 
-  return <div>Line Chart</div>
+// 准备数据
+const data = [
+  { genre: 'Sports', sold: 275 },
+  { genre: 'Strategy', sold: 115 },
+  { genre: 'Action', sold: 120 },
+  { genre: 'Shooter', sold: 350 },
+  { genre: 'Other', sold: 150 },
+]
+
+const Charts = () => {
+  const { darkMode, setDarkMode, locale, changeLocale } = useContext(GlobalContext)
+
+  const container = useRef(null)
+  const chart = useRef<Chart | null>(null)
+
+  useEffect(() => {
+    if (!chart.current) {
+      chart.current = renderBarChart(container.current)
+    } else {
+      chart.current.options({ theme: darkMode ? 'classicDark' : 'classic' })
+      chart.current.render()
+    }
+  }, [darkMode])
+
+  function renderBarChart(container: any) {
+    const chart = new Chart({
+      container,
+      theme: 'classic',
+    })
+
+    // 准备数据
+    const data = [
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: 120 },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 },
+    ]
+
+    // 声明可视化
+    chart
+      .interval() // 创建一个 Interval 标记
+      .data(data) // 绑定数据
+      .encode('x', 'genre') // 编码 x 通道
+      .encode('y', 'sold') // 编码 y 通道
+      .encode('key', 'genre') // 指定 key
+      .animate('enter', {
+        duration: 300,
+      })
+
+    chart.render()
+
+    return chart
+  }
+
+  // function updateBarChart(chart: Chart) {
+  //   // 获得 Interval Mark
+  //   const interval = chart.getNodesByType('interval')[0]
+  //
+  //   // 模拟并且更新 Interval 的数据
+  //   const newData = interval.data().map((d) => ({
+  //     ...d,
+  //     sold: Math.random() * 400 + 100,
+  //   }))
+  //
+  //   interval.data(newData)
+  //
+  //   // 重新渲染
+  //   chart.render()
+  // }
+
+  return (
+    <>
+      <div ref={container}></div>
+      {/*<button onClick={() => updateBarChart(chart.current)}>Update Data</button>*/}
+    </>
+  )
 }
 
 export default Charts

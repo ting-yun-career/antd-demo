6b6585e50ac68a4b68a8704ed1a5d57ce7e98df8
Ting Yun <ting.yun@binance.com>
2023-07-15 15:17:10 -0700

    i18n standard form

--- src/pages/Forms/Forms.tsx
+++ src/pages/Forms/Forms.tsx
@@ -12,9 +12,12 @@ import {
   Switch,
   TimePicker,
 } from 'antd'
+import { useContext } from 'react'
 import { PageTitle } from '../../components/PageTitle/PageTitle'
+import { GlobalContext } from '../../global/globalProvider'
 
 const Forms = () => {
+  const { locale } = useContext(GlobalContext)
   const [baiscForm] = Form.useForm()
   const [advancedForm] = Form.useForm()
 
@@ -22,23 +25,30 @@ const Forms = () => {
 
   const colResp = { xs: 24, sm: 24, md: 24, lg: 24, xl: 24, xxl: 24 }
 
+  const { RangePicker } = DatePicker
+
   return (
     <>
-      <PageTitle>Forms</PageTitle>
+      <PageTitle>{locale === 'zh_CN' ? '表单 - 标准格式' : 'Forms - Standard'}</PageTitle>
       <Row gutter={[10, 20]}>
         <Col {...colResp}>
-          <Card title="Basic Form Elements" style={{ width: '100%' }}>
+          <Card title={locale === 'zh_CN' ? '基层组件' : 'Basic Form Elements'} style={{ width: '100%' }}>
             <Form form={baiscForm} layout="vertical">
-              <Form.Item name="text" label="Text" style={formItemStyle}>
+              <Form.Item name="text" label={locale === 'zh_CN' ? '一般输入' : 'Text'} style={formItemStyle}>
                 <Input />
               </Form.Item>
-              <Form.Item name="password" label="Password" style={formItemStyle} hasFeedback>
+              <Form.Item
+                name="password"
+                label={locale === 'zh_CN' ? '密码键入' : 'Password'}
+                style={formItemStyle}
+                hasFeedback
+              >
                 <Input.Password />
               </Form.Item>
-              <Form.Item name="textarea" label="Textarea" style={formItemStyle}>
+              <Form.Item name="textarea" label={locale === 'zh_CN' ? '多行输入' : 'Textarea'} style={formItemStyle}>
                 <Input.TextArea showCount />
               </Form.Item>
-              <Form.Item name="select" label="Select" hasFeedback>
+              <Form.Item name="select" label={locale === 'zh_CN' ? '單选菜單' : 'Select'} hasFeedback>
                 <Select
                   placeholder="Please select"
                   options={[
@@ -48,7 +58,7 @@ const Forms = () => {
                   ]}
                 ></Select>
               </Form.Item>
-              <Form.Item name="multiselect" label="MultiSelect" hasFeedback>
+              <Form.Item name="multiselect" label={locale === 'zh_CN' ? '復选菜单' : 'MultiSelect'} hasFeedback>
                 <Select
                   mode={'multiple'}
                   placeholder="Please select"
@@ -59,7 +69,7 @@ const Forms = () => {
                   ]}
                 ></Select>
               </Form.Item>
-              <Form.Item name="autocomplete" label="AutoComplete">
+              <Form.Item name="autocomplete" label={locale === 'zh_CN' ? '自动筛选' : 'AutoComplete'}>
                 <AutoComplete
                   options={[
                     { label: 'Option A', value: 'A' },
@@ -72,13 +82,17 @@ const Forms = () => {
                   placeholder="try typing 'A' or 'B' or 'C'"
                 />
               </Form.Item>
-              <Form.Item name="numberValue" label="Numeral Value" style={formItemStyle}>
+              <Form.Item
+                name="numberValue"
+                label={locale === 'zh_CN' ? '数值输入' : 'Numeral Value'}
+                style={formItemStyle}
+              >
                 <InputNumber min={1} max={99} style={{ width: '100%' }} />
               </Form.Item>
-              <Form.Item name="toggle" label="Toggle" valuePropName="checked">
+              <Form.Item name="toggle" label={locale === 'zh_CN' ? '简易开关' : 'Toggle'} valuePropName="checked">
                 <Switch />
               </Form.Item>
-              <Form.Item name="slider" label="Slider">
+              <Form.Item name="slider" label={locale === 'zh_CN' ? '滑动取值' : 'Slider'}>
                 <Slider
                   marks={{
                     0: 'A',
@@ -94,17 +108,17 @@ const Forms = () => {
           </Card>
         </Col>
         <Col {...colResp}>
-          <Card title="Advanced Form Elements" style={{ width: '100%' }}>
+          <Card title={locale === 'zh_CN' ? '高級組件' : 'Advanced Form Elements'} style={{ width: '100%' }}>
             <Form form={advancedForm} layout="vertical">
-              <Form.Item name="date" label="Date Picker">
+              <Form.Item name="date" label={locale === 'zh_CN' ? '日期输入' : 'Date Picker'}>
                 <DatePicker style={{ width: '100%' }} />
               </Form.Item>
-              <Form.Item name="time" label="Time Picker">
+              <Form.Item name="time" label={locale === 'zh_CN' ? '时间输入' : 'Time Picker'}>
                 <TimePicker style={{ width: '100%' }} />
               </Form.Item>
-              {/*<Form.Item name="range" label="Date/Time Range Picker">*/}
-              {/*  <RangePicker style={{ width: '100%' }} />*/}
-              {/*</Form.Item>*/}
+              <Form.Item name="daterange" label={locale === 'zh_CN' ? '日期范围' : 'Date/Time Range Picker'}>
+                <RangePicker style={{ width: '100%' }} />
+              </Form.Item>
             </Form>
           </Card>
         </Col>

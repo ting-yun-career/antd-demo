2dd452b9e7eaca5605ae89f5adc072c03f900fab
Ting <ting.yun.career@gmail.com>
2023-08-22 11:10:13 -0700

    set default to chinese

--- src/global/globalProvider.tsx
+++ src/global/globalProvider.tsx
@@ -66,7 +66,7 @@ function GlobalContextProvider({ children }: { children: React.ReactNode }) {
 
   const { defaultAlgorithm, darkAlgorithm } = theme
   const [darkMode, setDarkMode] = useState(true)
-  const [locale, setLocale] = useState<string>('zh_CN')
+  const [locale, setLocale] = useState<string>('en_US')
   const [localeData, setLocaleData] = useState<Locale>(zhCN)
 
   const changeLocale = (locale: string) => {

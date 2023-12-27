562f4442af0c8fa10235cbf21caf66343b53e392
Ting <ting.yun.career@gmail.com>
2023-12-27 13:57:31 -0800

    tree update

--- src/pages/DSA/Tree/index.tsx
+++ src/pages/DSA/Tree/index.tsx
@@ -33,6 +33,11 @@ const Tree = () => {
     setVisiting(newVisiting)
   }
 
+  function reset() {
+    setVisiting(new Array(15).fill(false))
+    setVisited(new Array(15).fill(false))
+  }
+
   return (
     <>
       <PageTitle>{locale === 'zh_CN' ? '樹結構' : 'Tree Structure'}</PageTitle>
@@ -59,18 +64,36 @@ const Tree = () => {
       <div className={cls(['tf-tree', 'tf-gap-sm', { 'tf-dark': darkMode }])} style={{ width: '100%' }}>
         <ul>
           <li>
-            <span className={cls(['tf-nc', { 'tf-visiting': visiting[0] }])} onClick={(e) => onNodeClick(e, 0)}>
+            <span
+              className={cls(['tf-nc', { visiting: visiting[0], visited: visited[0] }])}
+              onClick={(e) => onNodeClick(e, 0)}
+            >
               {values[0]}
             </span>
             <ul>
               <li>
-                <span className="tf-nc">{values[1]}</span>
+                <span
+                  className={cls(['tf-nc', { visiting: visiting[1], visited: visited[1] }])}
+                  onClick={(e) => onNodeClick(e, 1)}
+                >
+                  {values[1]}
+                </span>
                 <ul>
                   <li>
-                    <span className="tf-nc">{values[3]}</span>
+                    <span
+                      className={cls(['tf-nc', { visiting: visiting[3], visited: visited[3] }])}
+                      onClick={(e) => onNodeClick(e, 3)}
+                    >
+                      {values[3]}
+                    </span>
                     <ul>
                       <li>
-                        <span className="tf-nc">{values[7]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[7], visited: visited[7] }])}
+                          onClick={(e) => onNodeClick(e, 7)}
+                        >
+                          {values[7]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -81,7 +104,12 @@ const Tree = () => {
                         </ul>
                       </li>
                       <li>
-                        <span className="tf-nc">{values[8]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[8], visited: visited[8] }])}
+                          onClick={(e) => onNodeClick(e, 8)}
+                        >
+                          {values[8]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -94,10 +122,20 @@ const Tree = () => {
                     </ul>
                   </li>
                   <li>
-                    <span className="tf-nc">{values[4]}</span>
+                    <span
+                      className={cls(['tf-nc', { visiting: visiting[4], visited: visited[4] }])}
+                      onClick={(e) => onNodeClick(e, 4)}
+                    >
+                      {values[4]}
+                    </span>
                     <ul>
                       <li>
-                        <span className="tf-nc">{values[9]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[9], visited: visited[9] }])}
+                          onClick={(e) => onNodeClick(e, 9)}
+                        >
+                          {values[9]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -108,7 +146,12 @@ const Tree = () => {
                         </ul>
                       </li>
                       <li>
-                        <span className="tf-nc">{values[10]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[10], visited: visited[10] }])}
+                          onClick={(e) => onNodeClick(e, 10)}
+                        >
+                          {values[10]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -123,13 +166,28 @@ const Tree = () => {
                 </ul>
               </li>
               <li>
-                <span className="tf-nc">{values[2]}</span>
+                <span
+                  className={cls(['tf-nc', { visiting: visiting[2], visited: visited[2] }])}
+                  onClick={(e) => onNodeClick(e, 2)}
+                >
+                  {values[2]}
+                </span>
                 <ul>
                   <li>
-                    <span className="tf-nc">{values[5]}</span>
+                    <span
+                      className={cls(['tf-nc', { visiting: visiting[5], visited: visited[5] }])}
+                      onClick={(e) => onNodeClick(e, 5)}
+                    >
+                      {values[5]}
+                    </span>
                     <ul>
                       <li>
-                        <span className="tf-nc">{values[11]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[11], visited: visited[11] }])}
+                          onClick={(e) => onNodeClick(e, 11)}
+                        >
+                          {values[11]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -140,7 +198,12 @@ const Tree = () => {
                         </ul>
                       </li>
                       <li>
-                        <span className="tf-nc">{values[12]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[12], visited: visited[12] }])}
+                          onClick={(e) => onNodeClick(e, 12)}
+                        >
+                          {values[12]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -153,10 +216,20 @@ const Tree = () => {
                     </ul>
                   </li>
                   <li>
-                    <span className="tf-nc">{values[6]}</span>
+                    <span
+                      className={cls(['tf-nc', { visiting: visiting[6], visited: visited[6] }])}
+                      onClick={(e) => onNodeClick(e, 6)}
+                    >
+                      {values[6]}
+                    </span>
                     <ul>
                       <li>
-                        <span className="tf-nc">{values[13]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[13], visited: visited[13] }])}
+                          onClick={(e) => onNodeClick(e, 13)}
+                        >
+                          {values[13]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -167,7 +240,12 @@ const Tree = () => {
                         </ul>
                       </li>
                       <li>
-                        <span className="tf-nc">{values[14]}</span>
+                        <span
+                          className={cls(['tf-nc', { visiting: visiting[14], visited: visited[14] }])}
+                          onClick={(e) => onNodeClick(e, 14)}
+                        >
+                          {values[14]}
+                        </span>
                         <ul>
                           <li>
                             <span className="tf-nc tf-null">❌</span>
@@ -184,6 +262,16 @@ const Tree = () => {
             </ul>
           </li>
         </ul>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button
+          className="primary"
+          onClick={() => {
+            reset()
+          }}
+        >
+          Reset
+        </button>
       </div>
     </>
   )
